a1b02cb6723948267bc8a8246aac869d5eb13c45
hinojoat <hinojoat@rose-hulman.edu>
2019-01-31 14:22:01 -0500

    WIP

--- src/m1e_root_and_mainloop.py
+++ src/m1e_root_and_mainloop.py
@@ -7,7 +7,7 @@ A window should pop up.  That's all for this demo.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
+         and Alex Hinojosa.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
